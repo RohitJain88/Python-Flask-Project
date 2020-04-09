9d26f5438c243cc0edd4c3110c2ab59ba94cfe65
RohitJain88 <rohit.jain88@yahoo.com>
2020-04-09 13:56:51 -0700

    Web forms and flask-security

--- enrollment/application/routes.py
+++ enrollment/application/routes.py
@@ -1,6 +1,7 @@
 from application import app, db
-from flask import render_template, request, json, Response
+from flask import render_template, request, json, Response, redirect, flash
 from application.models import User, Course, Enrollment
+from application.forms import LoginForm, RegisterForm
 
 courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
 
@@ -10,18 +11,26 @@ courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP",
 def index():
     return render_template("index.html", index=True)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", login=True)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if request.form.get("email") == 'test@uta.com':
+            flash("You are successfully logged in!", "success")
+            return redirect("/index")
+        else:
+            flash("Sorry! Something went wrong", "danger")
+    return render_template("login.html", form=form, title="Login", login=True)
 
 @app.route("/courses")
 @app.route("/courses/<term>")
 def courses(term="Spring 2019"):
     return render_template("courses.html", courseData=courseData, courses=True, term=term)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html", register=True)
+    form = RegisterForm()
+    return render_template("register.html", form=form, title="New User Registration", register=True)
 
 @app.route("/enrollment", methods=["GET", "POST"])
 def enrollment():
