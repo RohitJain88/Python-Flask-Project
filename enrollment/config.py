272b39e769ba9e6c041f54378981020608eb9440
RohitJain88 <rohit.jain88@yahoo.com>
2020-04-13 19:58:21 -0700

    Removing and untracking config.py for secret key

--- enrollment/config.py
+++ enrollment/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xd2\x1e@\x18\xeep\xe7\xd6c\x9fD&\xe5\xf6\xc2\x02'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret string"
 
     MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment'}
