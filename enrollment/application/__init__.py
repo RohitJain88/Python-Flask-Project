76a9c0c3da498e1806179415f6c210b64e019364
RohitJain88 <rohit.jain88@yahoo.com>
2020-04-15 22:21:01 -0700

    Rest API implementation using Flask Restplus

--- enrollment/application/__init__.py
+++ enrollment/application/__init__.py
@@ -3,13 +3,13 @@ from config import Config
 from flask_mongoengine import MongoEngine
 from flask_restplus import Api
 
-api = Api()
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = MongoEngine()
 db.init_app(app)
+
+api = Api()
 api.init_app(app)
 
 from application import routes
